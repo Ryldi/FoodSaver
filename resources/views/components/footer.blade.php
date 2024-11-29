3cbabaf42f169049ea99d101e4c4f985bb5f73fb
Ryldi <123353299+Ryldi@users.noreply.github.com>
2024-11-29 22:37:39 +0700

    chore: create view folder

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,12 @@
+<footer class="bg-accent/80">
+    <div class="container flex justify-center items-center p-6 gap-10 md:gap-20">
+        <div class="flex justify-center items-center gap-2 md:gap-6">
+            <img src="{{ asset('img/logo.png') }}" alt="logo" class="w-16 md:w-24">
+            <span class="text-white text-4xl md:text-5xl font-pattaya hover:text-accent-hover transition-all duration-500">Beeli</span>
+        </div>
+        <div class="flex flex-col md:flex-row md:gap-10 justify-center text-white items-center font-semibold">
+            <div class="text-2xl font-pattaya">Follow us on</div>
+            <a href="https://www.instagram.com/beeli.id" class="ig-icon"><img src="{{ asset('img/ig.before.png') }}" alt="instagram" class="w-8 md:w-10 transition-all duration-500"></a>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/layouts/dependency.blade.php
@@ -0,0 +1,8 @@
+<link rel="icon" href="{{ asset('img/logo.png') }}">
+<link href="https://fonts.googleapis.com/css2?family=Pattaya&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.css" rel="stylesheet" />
+<script src="https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.min.js" defer></script>
+
+@vite(['resources/css/app.css','resources/js/app.js'])
+<title>SisaSantap</title>
