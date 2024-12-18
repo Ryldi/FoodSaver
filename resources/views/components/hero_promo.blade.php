59442642d49e6de1cc498888c78bd8579ed4be50
ivanadriannn <ivanadrian0864@gmail.com>
2024-12-18 09:13:40 +0700

    fix: index, categoryDetail responsive

--- resources/views/components/hero_promo.blade.php
+++ resources/views/components/hero_promo.blade.php
@@ -1,11 +1,13 @@
+<br>
 <div class="container mx-auto ">
-    <div class="grid md:grid-cols-12 grid-rows-1 items-center gap-5 m">
-        <div class="flex justify-center col-span-5">
-            <img src="{{ asset('img/promo/promo1.png') }}" alt="">
+    <div class="flex flex-col md:flex-row gap-4 items-center justify-center col-span-2 w-full">
+        <div class="flex flex-col gap-2 w-full md:items-start col-span-12">
+            <img src="{{ asset('img/promo/promo1.png') }}" class="w-full h-auto object-cover" alt="">
+            <img src="{{ asset('img/promo/promo2.png') }}" class="w-full h-auto object-cover" alt="">
         </div>
-        <div class="flex flex-col gap-2 col-span-7">
-            <img src="{{ asset('img/promo/promo2.png') }}" alt="">
-            <img src="{{ asset('img/promo/promo3.png') }}" alt="">
+        <div class="flex flex-col gap-2 w-full md:items-start col-span-12">
+            <img src="{{ asset('img/promo/promo3.png') }}" class="w-full h-auto object-cover" alt="">
+            <img src="{{ asset('img/promo/promo4.png') }}" class="w-full h-auto object-cover" alt="">
         </div>
     </div>
 </div>
