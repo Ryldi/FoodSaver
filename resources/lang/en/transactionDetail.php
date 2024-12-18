2d27058ac6f9bed307296a9bbae90403c73f5dfa
ivanadriannn <ivanadrian0864@gmail.com>
2024-12-18 22:16:06 +0700

    feat: transactiondDetail-localization

---
+++ resources/lang/en/transactionDetail.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'header_detail_pemesanan' => 'Order Details',
+    'order_list' => 'Order List',
+    'payment_button' => 'Pay',
+    'status_paid' => 'Paid',
+    'status_unpaid' => 'Unpaid',
+    'status_prepared' => 'Prepared',
+    'prepare_order_button' => 'Prepare Order',
+    'complete_order_button' => 'Complete Order',
+    'pickup_location' => 'Pickup Location',
+    'order_details' => 'Order Details',
+    'total_product_price' => 'Total Product Price',
+    'total_price' => 'Total Price',
+];
---
+++ resources/lang/id/transactionDetail.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'header_detail_pemesanan' => 'Detail Pemesanan',
+    'order_list' => 'Daftar Pemesanan',
+    'payment_button' => 'Bayar',
+    'status_paid' => 'Lunas',
+    'status_unpaid' => 'Belum Dibayar',
+    'status_prepared' => 'Pesanan Siap',
+    'prepare_order_button' => 'Siapkan Pesanan',
+    'complete_order_button' => 'Selesaikan Pesanan',
+    'pickup_location' => 'Lokasi Penjemputan',
+    'order_details' => 'Rincian Pemesanan',
+    'total_product_price' => 'Total Harga Produk',
+    'total_price' => 'Total Harga',
+    'completed' => 'Selesai'
+];
